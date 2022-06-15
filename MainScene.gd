e456bd7068beb87148848a1771966925a55e0471
roi.mordechay <roi.mordechay@idealo.de>
2022-06-16 01:36:09 +0200

    First commit

---
+++ MainScene.gd
@@ -0,0 +1,9 @@
+extends Spatial
+
+func _ready():
+	print($Player/AnimationPlayer.)
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
